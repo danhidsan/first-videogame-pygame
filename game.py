883da3d604d032e7c056e0c6a6bb77494323e580
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2019-01-05 18:31:25 +0100

    Added window background color

--- game.py
+++ game.py
@@ -5,7 +5,10 @@ pygame.init()
 window = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("My first game")
 
+color = pygame.Color(70, 80, 150)
+
 while True:
+    window.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
